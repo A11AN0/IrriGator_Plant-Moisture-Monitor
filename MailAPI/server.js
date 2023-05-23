0c66469d5c36521c4d1112eaf62d22f4e06a3de5
Allan <matrixboston7613@gmail.com>
2023-05-24 03:04:56 +0800

    installed express - rest api functionality

--- MailAPI/server.js
+++ MailAPI/server.js
@@ -1,20 +1,30 @@
 require("dotenv").config();
 const formData = require("form-data");
 const Mailgun = require("mailgun.js");
-
+const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
     username: "api",
     key: process.env.MAILGUN_API_KEY,
 });
 
-mg.messages
-    .create(process.env.BASE_URL, {
-        from: "Excited User <IrriGator-UserAlerts@sandbox.mailgun.org>",
-        to: ["random@gmail.com"],
-        subject: "Hello",
-        text: "Testing some Mailgun awesomeness!",
-        html: "<h1>Testing some Mailgun awesomeness!</h1>",
-    })
-    .then((msg) => console.log(msg)) // logs response data
-    .catch((err) => console.log(err)); // logs any error
+// mg.messages
+//     .create(process.env.BASE_URL, {
+//         from: "Excited User <IrriGator-UserAlerts@sandbox.mailgun.org>",
+//         to: ["random@gmail.com"],
+//         subject: "Hello",
+//         text: "Testing some Mailgun awesomeness!",
+//         html: "<h1>Testing some Mailgun awesomeness!</h1>",
+//     })
+//     .then((msg) => console.log(msg)) // logs response data
+//     .catch((err) => console.log(err)); // logs any error
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+    console.log(`API server listening on port ${port}`);
+});
